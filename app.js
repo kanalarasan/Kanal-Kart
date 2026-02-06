942986b21c1dadd80cc5cd635756b1dd27297525
Super User <root@master.example.com>
2026-02-06 13:16:33 +0530

    ll files added

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Welcome to Kanal-Kart</h1>
+    <ul>
+      <li>Product 1: Phone</li>
+      <li>Product 2: Laptop</li>
+      <li>Product 3: Shoes</li>
+    </ul>
+  `);
+});
+
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
+
