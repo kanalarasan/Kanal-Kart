a550fe91d8a05d18dd68dfb61e0ddd8ed7f5b3c6
kanalarasan <41923214+kanalarasan@users.noreply.github.com>
2026-02-08 15:06:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
       <li>Product 2: Laptop</li>
       <li>Product 3: Shoes</li>
       <li>Prodcut 4: Watches</li>
-      <li>Prodcut 5: Bags.</li>
+      <li>Prodcut 5: Bag</li>
       </ul>
   `);
 });
